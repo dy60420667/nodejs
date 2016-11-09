667aeb2071ba13d9faf69498ca97bd68e04d2947
邓渊 <330715487@qq.com>
2016-11-09 08:32:00 +0800

    升级咯

--- HEJS/routes/index.js
+++ HEJS/routes/index.js
@@ -11,5 +11,21 @@ router.get('/task', function(req, res, next) {
   res.render('task', { title: '新闻客户端定制服务' });
 });
 
+/* GET home page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: '新闻客户端定制服务' });
+});
+
+/* GET home page. */
+router.get('/autosign', function(req, res, next) {
+  res.render('apktools/autosign', { title: '新闻客户端定制服务' });
+});
+
+/* GET home page. */
+router.get('/apktool', function(req, res, next) {
+  res.render('apktools/apktool', { title: '新闻客户端定制服务' });
+});
+
+
 
 module.exports = router;
--- HEJS/routes/task.js
+++ HEJS/routes/task.js
@@ -1,64 +1,28 @@
-// // var Task = require('./../models/Task.js');
-// var express = require('express');
-// var formidable = require('formidable');
-// var fs = require('fs');
-// var router = express.Router();
-// var os = require('os');
-
-// var handleAbout = function(req, res, next) {
-//   res.render('about', { title: '关于定制服务' });
-// };
-
-// var handleForm = function(res, req){
-//     var form        = new formidable.IncomingForm();
-// 	form.encoding = 'utf-8';        //设置编辑
-// 	form.uploadDir = 'public/images/tmp';     //设置上传目录
-// 	form.keepExtensions = true;     //保留后缀
-// 	form.maxFieldsSize = 2 * 1024 * 1024;   //文件大小
-
-// 	console.log('------form----');
-// 	console.log('------IncomingForm----');
-// 	console.log("req:"+req.toString());
-// 	console.log("req.body:"+req.body);
-// 	console.log("req.files:"+req.files);
-// 	console.log("req.params:"+req.params);
-// 	console.log("req.Headers:"+req.Headers);
-// 	console.log("req.query:"+req.query);
-// 	console.log('form:'+form.toString());
-// 	if (!fs.existsSync(form.uploadDir)) {
-// 		fs.mkdirSync(form.uploadDir);
-// 	}
-//     form.parse(req,function(error, fields, files){
-//     	console.log('------parse----');
-// 	    var types       = files.upload.name.split('.');
-// 	    var date        = new Date();
-// 	    var ms          = Date.parse(date);
-// 	    fs.renameSync(files.upload.path,"/tmp/files"+ ms +"."+String(types[types.length-1]));
-// 	    console.log('end prase');
-//     });
-//     console.log('上传成功');
-// }
-
-// /* GET home page. */
-// router.post('/addTask', handleForm);
-
-// /* GET home page. */
-// router.get('/about', handleAbout);
-// module.exports = router;
 var fs = require('fs');
 var os = require('os');
+var express = require('express');
+var router = express.Router();
 
 var handleFile = function(filename, file, targetDir) {
-    var targetFile = targetDir + filename;
+    // var targetFile = targetDir + filename;
+    var targetFile = './public/upload/'+filename;
+ 
     var fstream;
 
     console.log("Uploading: " + filename + ' to ' + targetFile);
     fstream = fs.createWriteStream(targetFile);
     file.pipe(fstream);
+    var passedLength = 0;
+   
+    fstream.on('data', function(chunk) {
+        passedLength += chunk.length;
+        console.log("已下载："+passedLength);
+
+    });
 
     fstream.on('close', function () {
         // Note to self: Fire off an event to handle the file in tmp here (check it, thumbs gen, record it, remove it)
-        console.log('Saved file: '+filename);
+        console.log('Saved file: '+targetFile);
     });
     fstream.on('error', function () {
         console.log('ERROR while saving file: '+filename);
@@ -104,5 +68,7 @@ var handleForm = function(req, res) {
 
 };
 
+/* GET home page. */
+router.post('/addTask', handleForm);
 
-module.exports = handleForm;
+module.exports = router;
